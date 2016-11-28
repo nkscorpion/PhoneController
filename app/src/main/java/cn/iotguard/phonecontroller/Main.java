a12dace0c589b890a41a75bcd14fbfc6539ba123
caowentao <caowentao@iotguard.cn>
2016-11-28 18:35:33 +0800

    基本完成，除了关闭网页时应用程序不能停止线程

--- app/src/main/java/cn/iotguard/phonecontroller/Main.java
+++ app/src/main/java/cn/iotguard/phonecontroller/Main.java
@@ -9,11 +9,10 @@ import android.support.v4.view.InputDeviceCompat;
 import android.view.InputEvent;
 import android.view.MotionEvent;
 
+import com.koushikdutta.async.callback.CompletedCallback;
 import com.koushikdutta.async.http.WebSocket;
 import com.koushikdutta.async.http.server.AsyncHttpServer;
 import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
-import com.koushikdutta.async.http.server.AsyncHttpServerResponse;
-import com.koushikdutta.async.http.server.HttpServerRequestCallback;
 
 import org.json.JSONObject;
 
@@ -36,20 +35,21 @@ public class Main {
     private static Method sInjectInputEventMethod;
     private static int sScreenWidth = 720;
     private static int sScreenHeight = 1280;
+    private static Thread sSendImageThread;
+
     public static void main(String[] args) {
         Looper.prepare();
-        System.out.println(
-                String.format(Locale.CHINA, "PhoneController start...%dx%d", sScreenWidth, sScreenHeight));
+        System.out.println(String.format(Locale.CHINA,
+                "PhoneController start...%dx%d", sScreenWidth, sScreenHeight));
         try {
             sInputManager = (InputManager) InputManager.class.getDeclaredMethod("getInstance").invoke(null);
             sInjectInputEventMethod = InputManager.class.getMethod("injectInputEvent", InputEvent.class, Integer.TYPE);
             AsyncHttpServer httpServer = new AsyncHttpServer();
-            httpServer.get("/screenshot.jpg", new RequestHandler());
             httpServer.websocket("/input", new InputHandler());
             httpServer.listen(LISTEN_PORT);
             Looper.loop();
         } catch (Exception e) {
-            System.out.print("error: " + e.getMessage());
+            System.out.println("error: " + e.getMessage());
         }
     }
 
@@ -57,55 +57,86 @@ public class Main {
         @Override
         public void onConnected(WebSocket webSocket, AsyncHttpServerRequest request) {
             System.out.println("websocket connected.");
-            webSocket.setStringCallback(new WebSocket.StringCallback() {
-                @Override
-                public void onStringAvailable(String s) {
-                    try {
-                        JSONObject touch = new JSONObject(s);
-                        float x = Float.parseFloat(touch.getString("x"))*1.5f;
-                        float y = Float.parseFloat(touch.getString("y"))*1.5f;
-                        String eventType = touch.getString(KEY_EVENT_TYPE);
-                        switch (eventType) {
-                            case KEY_FINGER_DOWN:
-                                injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 0,
-                                        SystemClock.uptimeMillis(), x, y, 1.0f);
-                                break;
-                            case KEY_FINGER_UP:
-                                injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 1,
-                                        SystemClock.uptimeMillis(), x, y, 1.0f);
-                                break;
-                            case KEY_FINGER_MOVE:
-                                injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 2,
-                                        SystemClock.uptimeMillis(), x, y, 1.0f);
-                                break;
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
+            if (sSendImageThread == null) {
+                sSendImageThread = new Thread(new SendScreenShotThread((webSocket)));
+                sSendImageThread.start();
+
+                webSocket.setClosedCallback(new CompletedCallback() {
+                    @Override
+                    public void onCompleted(Exception ex) {
+                        System.out.println("page closed : " + ex.getMessage());
+                        sSendImageThread.interrupt();
                     }
-                }
-            });
+                });
+
+                webSocket.setEndCallback(new CompletedCallback() {
+                    @Override
+                    public void onCompleted(Exception ex) {
+                        System.out.println("page closed : " + ex.getMessage());
+                        sSendImageThread.interrupt();
+                    }
+                });
+
+                webSocket.setStringCallback(new WebSocket.StringCallback() {
+                    @Override
+                    public void onStringAvailable(String s) {
+                        try {
+                            JSONObject touch = new JSONObject(s);
+                            float x = Float.parseFloat(touch.getString("x"))*1.5f;
+                            float y = Float.parseFloat(touch.getString("y"))*1.5f;
+                            String eventType = touch.getString(KEY_EVENT_TYPE);
+                            switch (eventType) {
+                                case KEY_FINGER_DOWN:
+                                    injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 0,
+                                            SystemClock.uptimeMillis(), x, y, 1.0f);
+                                    break;
+                                case KEY_FINGER_UP:
+                                    injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 1,
+                                            SystemClock.uptimeMillis(), x, y, 1.0f);
+                                    break;
+                                case KEY_FINGER_MOVE:
+                                    injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 2,
+                                            SystemClock.uptimeMillis(), x, y, 1.0f);
+                                    break;
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                });
+            } else {
+                webSocket.close();
+            }
         }
     }
 
-    private static class RequestHandler implements HttpServerRequestCallback {
-        public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
-            try {
-                String surfaceClassName;
-                if (Build.VERSION.SDK_INT <= 17) {
-                    surfaceClassName = "android.view.Surface";
-                } else {
-                    surfaceClassName = "android.view.SurfaceControl";
+    private static class SendScreenShotThread implements Runnable {
+
+        WebSocket mWebSocket;
+        String mSurfaceName;
+        SendScreenShotThread(WebSocket webSocket) {
+            mWebSocket = webSocket;
+            if (Build.VERSION.SDK_INT <= 17) {
+                mSurfaceName = "android.view.Surface";
+            } else {
+                mSurfaceName = "android.view.SurfaceControl";
+            }
+        }
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Bitmap bitmap = (Bitmap) Class.forName(mSurfaceName)
+                            .getDeclaredMethod("screenshot", new Class[]{Integer.TYPE, Integer.TYPE})
+                            .invoke(null, sScreenWidth / 2, sScreenHeight / 2);
+                    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bout);
+                    bout.flush();
+                    mWebSocket.send(bout.toByteArray());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    break;
                 }
-                Bitmap bitmap = (Bitmap) Class.forName(surfaceClassName)
-                        .getDeclaredMethod("screenshot", new Class[]{Integer.TYPE, Integer.TYPE})
-                        .invoke(null, sScreenWidth / 2, sScreenHeight / 2);
-                ByteArrayOutputStream bout = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bout);
-                bout.flush();
-                response.send("image/jpeg", bout.toByteArray());
-            } catch (Exception e) {
-                response.code(500);
-                response.send(e.toString());
             }
         }
     }
