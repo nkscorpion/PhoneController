104b02ffcda456611013c9c2707deef47b444317
caowentao <caowentao@iotguard.cn>
2016-11-28 15:20:28 +0800

    update

--- app/src/main/java/cn/iotguard/phonecontroller/Main.java
+++ app/src/main/java/cn/iotguard/phonecontroller/Main.java
@@ -1,10 +1,13 @@
 package cn.iotguard.phonecontroller;
 
 import android.graphics.Bitmap;
+import android.hardware.input.InputManager;
 import android.os.Build;
 import android.os.Looper;
 import android.os.SystemClock;
 import android.support.v4.view.InputDeviceCompat;
+import android.view.InputEvent;
+import android.view.MotionEvent;
 
 import com.koushikdutta.async.http.WebSocket;
 import com.koushikdutta.async.http.server.AsyncHttpServer;
@@ -15,6 +18,7 @@ import com.koushikdutta.async.http.server.HttpServerRequestCallback;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Method;
 import java.util.Locale;
 
 /**
@@ -28,7 +32,8 @@ public class Main {
     private static final String KEY_FINGER_UP = "fingerup";
     private static final String KEY_FINGER_MOVE = "fingermove";
     private static final String KEY_EVENT_TYPE = "type";
-    private static EventInput sEventInput;
+    private static InputManager sInputManager;
+    private static Method sInjectInputEventMethod;
     private static int sScreenWidth = 720;
     private static int sScreenHeight = 1280;
     public static void main(String[] args) {
@@ -36,16 +41,16 @@ public class Main {
         System.out.println(
                 String.format(Locale.CHINA, "PhoneController start...%dx%d", sScreenWidth, sScreenHeight));
         try {
-            sEventInput = new EventInput();
+            sInputManager = (InputManager) InputManager.class.getDeclaredMethod("getInstance").invoke(null);
+            sInjectInputEventMethod = InputManager.class.getMethod("injectInputEvent", InputEvent.class, Integer.TYPE);
+            AsyncHttpServer httpServer = new AsyncHttpServer();
+            httpServer.get("/screenshot.jpg", new RequestHandler());
+            httpServer.websocket("/input", new InputHandler());
+            httpServer.listen(LISTEN_PORT);
+            Looper.loop();
         } catch (Exception e) {
-            System.out.print("EventInput error: " + e.getMessage());
-            sEventInput = null;
+            System.out.print("error: " + e.getMessage());
         }
-        AsyncHttpServer httpServer = new AsyncHttpServer();
-        httpServer.get("/screenshot.jpg", new RequestHandler());
-        httpServer.websocket("/input", new InputHandler());
-        httpServer.listen(LISTEN_PORT);
-        Looper.loop();
     }
 
     private static class InputHandler implements AsyncHttpServer.WebSocketRequestCallback {
@@ -62,15 +67,15 @@ public class Main {
                         String eventType = touch.getString(KEY_EVENT_TYPE);
                         switch (eventType) {
                             case KEY_FINGER_DOWN:
-                                sEventInput.injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 0,
+                                injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 0,
                                         SystemClock.uptimeMillis(), x, y, 1.0f);
                                 break;
                             case KEY_FINGER_UP:
-                                sEventInput.injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 1,
+                                injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 1,
                                         SystemClock.uptimeMillis(), x, y, 1.0f);
                                 break;
                             case KEY_FINGER_MOVE:
-                                sEventInput.injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 2,
+                                injectMotionEvent(InputDeviceCompat.SOURCE_TOUCHSCREEN, 2,
                                         SystemClock.uptimeMillis(), x, y, 1.0f);
                                 break;
                         }
@@ -102,6 +107,17 @@ public class Main {
                 response.code(500);
                 response.send(e.toString());
             }
+        }
+    }
+
+    private static void injectMotionEvent(int inputSource, int action, long when, float x, float y, float pressure) {
+        try {
+            MotionEvent event = MotionEvent.obtain(when, when, action, x, y, pressure, 1.0f, 0, 1.0f, 1.0f, 0, 0);
+            event.setSource(inputSource);
+            sInjectInputEventMethod.invoke(sInputManager, event, 0);
+            event.recycle();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
